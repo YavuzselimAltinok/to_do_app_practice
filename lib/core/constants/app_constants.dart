9d61e8fbeaf28f437b3bb27f81d15422ee4bed98
Mahammad (Rohtolos) Osmanov <m.osmanovdev@gmail.com>
2025-06-26 07:55:45 +0400

    restart project

--- lib/core/constants/app_constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  static const String tasksBox = "tasks_box";
-  static const String tasksNameListKey = "tasksNameList";
-  static const String tasksIsDoneListKey = "tasksIsDoneList";
-  static const String subTasksNameListKey = "subTasksNameList";
-}
